090c624399fdf1a426ed1589cb9b78bd1c379907
bogdanutanu <bogdan.utanu.ncr@gmail.com>
2016-07-20 12:02:13 +0100

    Transactions list and balance computation working

--- src/components/Main.js
+++ src/components/Main.js
@@ -33,21 +33,23 @@ class LikeButton extends React.Component {
   }
 }
 
-var TransactionBox = React.createClass({
-  handleTransactionSubmit: function(transaction) {
-    newTransactions = transactions.concat([transaction]);
-    newBalance = balance + transaction.amount;
-    this.setState({transactions: newTransactions, balance: newBalance});
-  },
-  getInitialState: function() {
-    return {transactions: [], balance: 0};
-  },
+setInterval(function() {
+  ReactoDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('app')
+  );
+}, 500);
+
+ReactoDOM.render(
+  <LikeButton />,
+  document.getElementById('like')
+);
+
+
+var Transaction = React.createClass({
   render: function() {
     return (
-      <div className="transactionBox">
-        <h2>Your balance:</h2>
-        <span>{this.state.balance}</span>
-      </div>
+      <tr><td>{this.props.description}</td><td>{this.props.amount}</td></tr>
     );
   }
 });
@@ -61,21 +63,81 @@ var TransactionsList = React.createClass({
     );
     });
     return (
-      <div className="transactionList">
-        {}
-      </div>
+      <table className="transactionList">
+        <thead><tr><th>Description</th><th>Amount</th></tr></thead>
+        <tbody>{transactionNodes}</tbody>
+      </table>
     );
   }  
 });
 
-setInterval(function() {
-  ReactoDOM.render(
-    <HelloWorld date={new Date()} />,
-    document.getElementById('app')
-  );
-}, 500);
+var TransactionBox = React.createClass({
+  handleTransactionSubmit: function(transaction) {
+    var transactions = this.state.transactions;
+    var newTransactions = transactions.concat([transaction]);
+
+    var balance = this.state.balance;
+    var newBalance = balance + transaction.amount;
+    this.setState({transactions: newTransactions, balance: newBalance});
+  },
+  getInitialState: function() {
+    return {transactions: [], balance: 0};
+  },
+  render: function() {
+    return (
+      <div className="transactionBox">
+        <h2>Your balance:</h2>
+        <span>{this.state.balance}</span>
+        <TransactionsList data={this.state.transactions} />
+        <TransactionForm onTransactionSubmit={this.handleTransactionSubmit} />
+      </div>
+    );
+  }
+});
+
+var TransactionForm = React.createClass({
+  getInitialState: function(){
+    return {description: '', 
+      amount: ''};
+  },
+  handleDescriptionChange: function(e){
+    this.setState({description: e.target.value});
+  },
+  handleAmountChange: function(e){
+    this.setState({amount: e.target.value});
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var description = this.state.description.trim();
+    var amount = this.state.amount.trim();
+    if (!description || !amount || isNaN(amount)) {
+      return;
+    }
+    this.props.onTransactionSubmit({description: description, amount: Number(amount)});
+    this.setState({description: '', amount: ''});
+  },
+  render: function(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Transaction description"
+          value={this.state.description}
+          onChange={this.handleDescriptionChange}
+        />
+        <input
+          type="text"
+          placeholder="Amount to be added or deducted. If deducted, must be preceeded by a - symbol"
+          value={this.state.amount}
+          onChange={this.handleAmountChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
 
 ReactoDOM.render(
-  <LikeButton />,
-  document.getElementById('like')
+  <TransactionBox />,
+  document.getElementById('transactions')
 );
